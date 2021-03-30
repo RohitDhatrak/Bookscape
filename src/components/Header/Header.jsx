942b7eb0e556efcd5b426dbda62d4ec6d2b19e78
Rohit Dhatrak <57338980+rohit436@users.noreply.github.com>
2021-03-30 23:45:51 +0530

    added header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import logo from "../../assets/logo.png";
+import { HeartSvg, Cart, SearchSvg, MenuSvg } from "../Helpers/Svg";
+
+export function Header() {
+    return (
+        <header className="nav flex-wrap">
+            <div>{<MenuSvg />}</div>
+            <a className="nav-brand-link" href="./index.js">
+                <span>
+                    <img className="nav-brand-logo" src={logo} alt="" />
+                    <span className="nav-brand-name">Bookoe</span>
+                </span>
+            </a>
+
+            <div className="nav-search">
+                <input type="text" />
+                {<SearchSvg />}
+            </div>
+
+            <ul className="nav-link-section">
+                <li className="nav-link nav-btns">{<HeartSvg />}</li>
+                <li className="nav-link nav-btns">{<Cart />}</li>
+                <figure className="avatar avatar-std avatar-square">
+                    <img
+                        src="https://pbs.twimg.com/profile_images/1140298044543401985/bcpbMyIV_400x400.jpg"
+                        alt="profile"
+                    />
+                </figure>
+            </ul>
+        </header>
+    );
+}
