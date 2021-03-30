3d450366abc98fb9b734c282e5ff7cb369326a9a
Rohit Dhatrak <57338980+rohit436@users.noreply.github.com>
2021-03-30 23:48:39 +0530

    created a page for storing all the common data

---
+++ src/components/Helpers/data.js
@@ -0,0 +1,33 @@
+export const sortByList = [
+    "Relevance",
+    "Popularity",
+    "Newest Arrivals",
+    "Price: Low to High",
+    "Price: High to Low",
+];
+
+export const filterObj = {
+    Genre: [
+        "All Genres",
+        "Biography",
+        "Business",
+        "Fantasy",
+        "Fiction",
+        "Historical Fiction",
+        "Humor and Comedy",
+        "Mystery",
+        "Nonfiction",
+    ],
+    Author: [
+        "Napoleon Hill",
+        "J.K Rowling",
+        "Dr Joseph Murphy",
+        "Dale Carnegie",
+        "George S. Clason",
+        "Francesc Miralles",
+        "Hector Garcia",
+    ],
+    Review: ["⭐⭐⭐⭐ & Up", "⭐⭐⭐ & Up", "⭐⭐ & Up", "⭐ & Up"],
+    Price: [0, 100, 200, 500, 1000, Number.MAX_SAFE_INTEGER],
+    Delivery: ["Get it by Tomorrow", "Get it in 2 Days", "Cash on Delivery"],
+};
