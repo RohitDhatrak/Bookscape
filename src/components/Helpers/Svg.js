eb3920419b6be9e44b6c5d8a41e4ac619bba8b68
Rohit Dhatrak <57338980+rohit436@users.noreply.github.com>
2021-04-01 11:17:11 +0530

    added dropdown svg

--- src/components/Helpers/Svg.js
+++ src/components/Helpers/Svg.js
@@ -94,3 +94,14 @@ export function FilterSvg() {
         </svg>
     );
 }
+
+export function DownSvg() {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 24 24">
+            <path
+                d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6l-6-6l1.41-1.42z"
+                fill="currentColor"
+            ></path>
+        </svg>
+    );
+}
