d46df9bec1619e535c110fc15077bc968c9b48ce
Rohit Dhatrak <57338980+rohit436@users.noreply.github.com>
2021-04-02 18:59:09 +0530

    added svg styling

--- src/components/Helpers/Svg.js
+++ src/components/Helpers/Svg.js
@@ -1,11 +1,6 @@
 export function HeartSvg() {
     return (
-        <svg
-            width="1em"
-            height="1em"
-            viewBox="0 0 24 24"
-            style={{ transform: "scale(1.2)" }}
-        >
+        <svg width="1em" height="1em" viewBox="0 0 24 24" className="heart-svg">
             <path
                 d="M12.1 18.55l-.1.1l-.11-.1C7.14 14.24 4 11.39 4 8.5C4 6.5 5.5 5 7.5 5c1.54 0 3.04 1 3.57 2.36h1.86C13.46 6 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5c0 2.89-3.14 5.74-7.9 10.05M16.5 3c-1.74 0-3.41.81-4.5 2.08C10.91 3.81 9.24 3 7.5 3C4.42 3 2 5.41 2 8.5c0 3.77 3.4 6.86 8.55 11.53L12 21.35l1.45-1.32C18.6 15.36 22 12.27 22 8.5C22 5.41 19.58 3 16.5 3z"
                 fill="currentColor"
@@ -20,7 +15,7 @@ export function FilledHertSvg() {
             width="1em"
             height="1em"
             viewBox="0 0 24 24"
-            style={{ transform: "scale(1.2)" }}
+            className="filled-heart-svg"
         >
             <path
                 d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5C2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53L12 21.35z"
@@ -32,12 +27,7 @@ export function FilledHertSvg() {
 
 export function CartSvg() {
     return (
-        <svg
-            width="1em"
-            height="1em"
-            viewBox="0 0 24 24"
-            style={{ transform: "scale(1.2)" }}
-        >
+        <svg width="1em" height="1em" viewBox="0 0 24 24" className="cart-svg">
             <path
                 d="M15.55 13c.75 0 1.41-.41 1.75-1.03l3.58-6.49A.996.996 0 0 0 20.01 4H5.21l-.94-2H1v2h2l3.6 7.59l-1.35 2.44C4.52 15.37 5.48 17 7 17h12v-2H7l1.1-2h7.45zM6.16 6h12.15l-2.76 5H8.53L6.16 6zM7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2s-.9-2-2-2zm10 0c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2s2-.9 2-2s-.9-2-2-2z"
                 fill="currentColor"
@@ -52,7 +42,7 @@ export function SearchSvg() {
             width="1em"
             height="1em"
             viewBox="0 0 24 24"
-            style={{ transform: "scale(1.1)", cursor: "pointer" }}
+            className="search-svg"
         >
             <path
                 d="M20.29 18.88l-5.56-5.56c1.13-1.55 1.63-3.58.98-5.74c-.68-2.23-2.57-3.98-4.85-4.44a6.511 6.511 0 0 0-7.72 7.72c.46 2.29 2.21 4.18 4.44 4.85c2.16.65 4.19.15 5.74-.98l5.56 5.56a.996.996 0 1 0 1.41-1.41zM5 9.5C5 7.01 7.01 5 9.5 5S14 7.01 14 9.5S11.99 14 9.5 14S5 11.99 5 9.5z"
@@ -100,6 +90,22 @@ export function DownSvg() {
         <svg width="1em" height="1em" viewBox="0 0 24 24">
             <path
                 d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6l-6-6l1.41-1.42z"
+                fill="currentColor"
+            ></path>
+        </svg>
+    );
+}
+
+export function BackArrowSvg() {
+    return (
+        <svg
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+            className="back-arrow-svg"
+        >
+            <path
+                d="M20 11H7.83l5.59-5.59L12 4l-8 8l8 8l1.41-1.41L7.83 13H20v-2z"
                 fill="currentColor"
             ></path>
         </svg>
