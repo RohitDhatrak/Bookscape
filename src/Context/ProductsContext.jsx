7888e5dac23ee0b324d8231ff35843e18d71bfff
Rohit Dhatrak <57338980+rohit436@users.noreply.github.com>
2021-04-02 11:47:50 +0530

    moved the reducer to context

--- src/Context/ProductsContext.jsx
+++ src/Context/ProductsContext.jsx
@@ -1,15 +1,23 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useReducer } from "react";
+import { reducer, initialState } from "../Reducer/ProductsReducer";
 const ProductsContext = createContext();
 
 export function ProductsContextProvider({ children }) {
     const [showSortPage, setShowSortPage] = useState(false);
     const [showFilterPage, setShowFilterPage] = useState(false);
+    const [selectedSort, setSelectedSort] = useState("Best Selling");
+    const [{ filterBy, sortBy }, dispatch] = useReducer(reducer, initialState);
 
     const data = {
         showSortPage,
         setShowSortPage,
         showFilterPage,
         setShowFilterPage,
+        selectedSort,
+        setSelectedSort,
+        filterBy,
+        sortBy,
+        dispatch,
     };
 
     return (
