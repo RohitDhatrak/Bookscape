99a94d550ad68a8ac5a9d3d1bdfa8d126eb4fbbb
Rohit Dhatrak <57338980+rohit436@users.noreply.github.com>
2021-04-06 19:24:34 +0530

    added routes for other pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { ProductListing } from "./Pages/ProductListing/ProductListing";
+import { WishList } from "./Pages/WishList/WishList";
+import { Checkout } from "./Pages/Checkout/Checkout";
 import { ProductsContextProvider } from "./Context/ProductsContext";
 import { useRouteContext } from "./Context/RouteContext";
+import { ReducerContextProvider } from "./Context/ReducerContext";
 
 export function App() {
     const { route } = useRouteContext();
 
     return (
         <div>
-            <ProductsContextProvider>
-                {route === "products" && <ProductListing />}
-            </ProductsContextProvider>
-            {/* {route === "cart" && } */}
-            {/* {route === "wishlist && "} */}
+            <ReducerContextProvider>
+                <ProductsContextProvider>
+                    {route === "products" && <ProductListing />}
+                </ProductsContextProvider>
+                {route === "wishlist" && <WishList />}
+                {route === "cart" && <Checkout />}
+            </ReducerContextProvider>
         </div>
     );
 }
