db169dd4776844f569083984f14f00069a2b6233
Rohit Dhatrak <57338980+rohit436@users.noreply.github.com>
2021-04-15 01:29:51 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ProductListing } from "./Pages/ProductListing/ProductListing";
 import { WishList } from "./Pages/WishList/WishList";
 import { Cart } from "./Pages/Cart/Cart";
@@ -9,6 +9,7 @@ import { Page404 } from "./Pages/404/Page404";
 import { Routes, Route } from "react-router-dom";
 import { Login } from "./Pages/Login/Login";
 import { Signup } from "./Pages/Signup/Signup";
+import { Home } from "./Pages/Home/Home";
 
 function ProductsPageWithContext() {
     return (
@@ -19,17 +20,17 @@ function ProductsPageWithContext() {
 }
 
 export function App() {
-    const loggedIn = true;
     return (
         <ReducerContextProvider>
             <Routes>
-                <Route path="/" element={<ProductsPageWithContext />} />
+                <Route path="/" element={<Home />} />
                 <Route path="/wishlist" element={<WishList />} />
+                <Route path="/products" element={<ProductsPageWithContext />} />
                 <Route path="/cart" element={<Cart />} />
                 <Route path="/product/:bookId" element={<ProductDetails />} />
                 <Route path="*" element={<Page404 />} />
-                {loggedIn && <Route path="/login" element={<Login />} />}
                 <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </ReducerContextProvider>
     );
